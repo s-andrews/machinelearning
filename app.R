54d41db0599824a8dd303d32f7b0395ebe78d897
laurabiggins <laurabiggins10@gmail.com>
2023-08-18 11:31:29 +0100

    Updated DT code to match optimising app

--- app.R
+++ app.R
@@ -130,36 +130,23 @@ server <- function(input, output) {
       
   # Output tables ----
   output$test_original_data <- DT::renderDataTable(
-    pivot_counts(training_counts()), 
-    caption = "Original training data", 
-    rownames = FALSE, 
-    options = list(dom = "t")
+    counts_table(training_counts(), title = "Original training data") 
   )
   
-  output$test_original_correct <- DT::renderDataTable(
-    training_summary(), 
-    caption = "Summary of training data", 
-    rownames = FALSE, 
-    options = list(dom = "t")
-  )
+  output$test_original_correct <- DT::renderDataTable({
+    summary_metrics_table(training_summary(), title = "Summary of training data")
+  })
   
   output$test_new_data <- DT::renderDataTable(
-    pivot_counts(test_counts()), 
-    caption = "New test data", 
-    rownames = FALSE, 
-    options = list(dom = "t")
+    counts_table(test_counts(), title = "New test data")
   )
   
   output$test_new_correct <- DT::renderDataTable(
-    test_summary(), 
-    caption = "Summary of test data", 
-    rownames = FALSE, 
-    options = list(dom = "t")
+    summary_metrics_table(test_summary(), title = "Summary of test data")
   )
   
   # Output text ----
   output$model_info1 <- renderPrint({
-    h2("Model information")
     model() %>% translate()
   })
 
